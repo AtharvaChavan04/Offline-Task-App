d44232d1fe78c759b43a37b87a5f83bf698707a8
unknown <mvmatharva4@gmail.com>
2025-09-05 12:10:51 +0530

    initial commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
     res.send('Hello, World!! This is my backend server.');
 })
 
-app.listen(8000, () => {
-    console.log('Server is running on http://localhost:8000');
+app.listen(8000, "0.0.0.0", () => {
+    console.log("Server is running on http://0.0.0.0:8000");
 });
